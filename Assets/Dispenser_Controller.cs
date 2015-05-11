72d437880e0bdca29589e234aea14cb47a4244c8
José Omar Colorado Pérez <jose.tatin@hotmail.com>
2015-05-10 22:42:29 -0500

    Versión 0.1
    Se incluyen animaciones de abrir llave, llave abierta y cerrada,
    dispensar gel.

---
+++ Assets/Dispenser_Controller.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dispenser_Controller : MonoBehaviour {
+
+	public ParticleSystem gel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown ("g")) {
+			gel.Play();
+		}
+
+	}
+}
---
+++ Assets/Scripts/Sink_Water.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sink_Water : MonoBehaviour {
+
+	public ParticleSystem water;
+	public GameObject sinkWater;
+	public Animation anim;
+	private bool close = true;
+	
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	void OnMouseDown()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!anim.isPlaying) {
+			
+			if(Input.GetKeyDown("o") && close == true)
+			{
+				anim.Blend ("Handle_Open");
+				close = false;
+				water.Play();
+			}
+			else if(Input.GetKeyDown("c") && close == false)
+			{
+				anim.Blend("Handle_Close");
+				water.Stop();
+				close = true;
+			}
+			
+		}
+	}
+
+	void PlayAnim(string s)
+	{
+
+		anim.Blend (s);
+	}
+}
